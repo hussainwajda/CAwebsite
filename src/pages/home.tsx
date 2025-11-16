cd590cce79e84e8f5d627ce382c443f930ca0451
hussain <hussainwajda786@gmail.com>
2025-11-16 22:11:09 +0530

    amplify updates

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import HeroSection from '@/components/home/HeroSection'
 import AboutSection from '@/components/home/about'
 import ServicesCarousel from '@/components/home/services'
